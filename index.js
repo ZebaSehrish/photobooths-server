f39e01919ae20d940c100e40e93f0df8951d8aca
Sehrish <sehrishzeba012@gmail.com>
2022-11-09 18:05:22 +0600

    6th

--- index.js
+++ index.js
@@ -47,12 +47,38 @@ async function run() {
             res.send(orders);
         });
 
-
         app.post('/reviews', async (req, res) => {
             const order = req.body;
             const result = await reviewCollection.insertOne(order);
             res.send(result);
+        });
+
+        app.get('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await reviewCollection.findOne(query);
+            res.send(result);
+        });
+
+        app.patch('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const status = req.body.status;
+            const query = { _id: ObjectId(id) };
+            const updatedDoc = {
+                $set: {
+                    status: status
+                }
+            }
+            const result = await reviewCollection.updateOne(query, updatedDoc);
+            res.send(result);
         })
+
+        app.delete('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await reviewCollection.deleteOne(query);
+            res.send(result);
+        });
 
     }
     finally {
